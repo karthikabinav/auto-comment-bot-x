99a0e08fb53703a8392231f4606e8046cf08753e
Karthik Abinav Sankararaman <karthikabinavs@gmail.com>
2026-09-19 14:17:13 -0700

    Add automation script to comment on new issues

--- auto_comment.py
+++ auto_comment.py
@@ -1,19 +1,19 @@
-#!/usr/bin/env python3
-"""Automatically adds a comment to any new issue."""
+"""Script that automatically adds a comment to any new issue."""
 import os
 from github import Github
 
-COMMENT = "Thank you for your contribution!"
+COMMENT_BODY = "Thank you for your contribution!"
 
 def main():
     token = os.environ.get("GITHUB_TOKEN")
     repo_name = os.environ.get("GITHUB_REPOSITORY")
     issue_number = int(os.environ.get("ISSUE_NUMBER", "0"))
-    if token and repo_name and issue_number:
-        gh = Github(token)
-        repo = gh.get_repo(repo_name)
-        issue = repo.get_issue(number=issue_number)
-        issue.create_comment(COMMENT)
+    if not token or not repo_name or not issue_number:
+        return
+    gh = Github(token)
+    repo = gh.get_repo(repo_name)
+    issue = repo.get_issue(number=issue_number)
+    issue.create_comment(COMMENT_BODY)
 
 if __name__ == "__main__":
     main()
